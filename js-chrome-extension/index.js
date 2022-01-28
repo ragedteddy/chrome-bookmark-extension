5f117c1de296e41c1e215ea74c86d2d58f96d39f
ragedteddy <55912131+ragedteddy@users.noreply.github.com>
2022-01-28 08:13:43 +0530

    Add files via upload

--- js-chrome-extension/index.js
+++ js-chrome-extension/index.js
@@ -1,13 +1,30 @@
 let myBMarks = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const deleteBtn = document.getElementById("delete-btn");
 const uList = document.getElementById("ulist");
+const tabBtn = document.getElementById("tab-btn");
+
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        myBMarks.push(tabs[0].url);
+        localStorage.setItem("bMarks", JSON.stringify(myBMarks));
+        renderList();
+    })
+
+})
 
 inputBtn.addEventListener("click", () => {
     myBMarks.push(inputEl.value);
     localStorage.setItem("bMarks", JSON.stringify(myBMarks));
     renderList();
     inputEl.value = "";
+})
+
+deleteBtn.addEventListener("dblclick", () => {
+    myBMarks = [];
+    localStorage.setItem("bMarks", JSON.stringify(myBMarks));
+    renderList();
 })
 
 function renderList() {
